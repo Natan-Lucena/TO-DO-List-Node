84f8b9ee78f46a47fdaa1057a4f8e32f0a331ba0
Natan <zerogamerptbr@gmail.com>
2023-08-05 01:34:36 -0300

    Solving a connection error

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -3,24 +3,28 @@ const connect = require('./elephantsql');
 const getAll = async() => {
     const client = await connect();
     const tasks = await client.query('SELECT * FROM tasks');
+    client.release();
     return tasks;
 };
 
 const insertData = async(nome, hora) => {
     const client = await connect();
     await client.query('INSERT INTO tasks (nome, hora) VALUES ($1, $2)', [nome, hora]);
+    client.release();
     return;
 };
 
 const deleteData = async(nome) => {
     const client = await connect();
     await client.query('DELETE FROM tasks WHERE nome = $1', [nome]);
+    client.release();
     return;
 };
 
 const updateTask = async(nome, hora , id) => {
     const client = await connect();
     await client.query('UPDATE tasks SET hora = $1, nome = $2 WHERE id = $3', [hora, nome,id]);
+    client.release();
     return;
 };
 
