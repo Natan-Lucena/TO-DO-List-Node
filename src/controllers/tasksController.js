c308e14be0927483d4864d4967f85a4d785bd043
Natan <zerogamerptbr@gmail.com>
2023-08-06 00:41:08 -0300

    Architeture problems

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,29 +1,49 @@
 const tasksModel = require('../models/tasksModel');
 
 const getAll = async (req,res) => {
-    
-    const tasks = await tasksModel.getAll();
-    
-    return tasks;
+    try{
+        const tasks = await tasksModel.getAll();
+        console.log(tasks.rows);
+        res.status(200).json(tasks.rows);
+    }catch(error){
+        console.error('Erro ao executar a consulta:', error);
+        res.status(500).send('Erro ao consultar o banco de dados');
+    }
 };
 
 const insertData = async (req,res) => {
-    const { nome, hora } = req.params;
-    await tasksModel.insertData(nome, hora);
-    return;
+    try{
+        const { nome, hora } = req.body;
+        await tasksModel.insertData(nome, hora);
+        res.status(200).send('Tarefa inserido com sucesso!');
+    }catch(error){
+        console.error('Erro ao executar a consulta:', error);
+        res.status(500).send('Erro ao consultar o banco de dados');
+    }
 };
 
 const deleteData = async (req,res) => {
-    const { nome } = req.params;
-    await tasksModel.deleteData(nome);
-    return;
+    try{
+        const { nome } = req.body;
+        console.log(nome)
+        await tasksModel.deleteData(nome);
+        res.status(200).send('Tarefa excluida com sucesso!');
+    } catch(error){
+        console.error('Erro ao executar a consulta:', error);
+        res.status(500).send('Erro ao consultar o banco de dados');
+    }
 };
 
 const updateTask = async (req,res) => {
-    const {nome,hora,id} = req.params;
-    await tasksModel.updateTask(nome,hora,id);
-    return;
-}
+    try{
+        const {nome,hora,id} = req.body;
+        await tasksModel.updateTask(nome,hora,id);
+        res.status(200).send('Tarefa atualizada com sucesso!');
+    }catch(error){
+        console.error('Erro ao executar a consulta:', error);
+        res.status(500).send('Erro ao consultar o banco de dados');
+    }   
+};
 
 module.exports = {
     getAll,
--- src/router.js
+++ src/router.js
@@ -1,46 +1,12 @@
 const express = require('express');
 const tasksController = require('./controllers/tasksController');
-const { insertData, deleteData, updateTask } = require('./models/tasksModel');
 
 const router = express.Router();
 
-router.post('/insert', async (req, res) => {
-  try{
-    await insertData(req.body.nome, req.body.hora);
-    res.status(200).send('Tarefa inserido com sucesso!');
-  } catch (error) {
-    console.error('Erro ao executar a consulta:', error);
-    res.status(500).send('Erro ao consultar o banco de dados');
-  }});
+router.post('/insert', tasksController.insertData);
+router.get('/tasks',  tasksController.getAll);
+router.delete('/delete', tasksController.deleteData);
+router.put('/update', tasksController.updateTask);
 
-router.get('/tasks', async (req, res) => {
-  try{
-    let tasks = await tasksController.getAll();
-     console.log(tasks.rows);
-     res.status(200).json(tasks.rows);
-  }
-    catch (error) {
-    console.error('Erro ao executar a consulta:', error);
-    res.status(500).send('Erro ao consultar o banco de dados');
-}});
-
-router.delete('/delete', async (req, res) => {
- try{
-    console.log(req.body.nome);
-    await deleteData(req.body.nome);
-    res.status(200).send('Tarefa excluida com sucesso!');
-  } catch (error) {
-    console.error('Erro ao executar a consulta:', error);
-    res.status(500).send('Erro ao consultar o banco de dados');
-}});
-
-router.put('/update', async (req,res) => {
-  try{
-    updateTask(req.body.nome,req.body.hora,req.body.id); 
-    res.status(200).send('Tarefa atualizada com sucesso!');
-  } catch (error) { 
-    console.error('Erro ao executar a consulta:', error);
-    res.status(500).send('Erro ao consultar o banco de dados');
-}});
 
 module.exports = router;
