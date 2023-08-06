eeb146c95bb8aa82ef1464b60169cc192df3f525
Natan <zerogamerptbr@gmail.com>
2023-08-06 16:49:15 -0300

    Adding new feature

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,29 @@
 const authModel = require('../models/authModel');
 
 const validateData = async (req, res) => {
-    const {user, email, password, confirmPassword} = req.body
-
-    if(!user || !email || !password || !confirmPassword ){
-        return res.status(422).json({msg : "Something is missing!"})
-    };
-    if(password !== confirmPassword){
-        return res.status(422).json({msg: "The Passwords doesn't match"})
-    };
-    const userFinder = await authModel.findByUser(req.body.user);
-    if(userFinder.length > 0){
-        return res.status(422).json({msg: "This user already exists"})
+    try{
+        const {user, email, password, confirmPassword} = req.body
+    
+        if(!user || !email || !password || !confirmPassword ){
+            return res.status(422).json({msg : "Something is missing!"})
+        };
+        if(password !== confirmPassword){
+            return res.status(422).json({msg: "The Passwords doesn't match"})
+        };
+        const userFinder = await authModel.findByUser(user);
+        if(userFinder.length > 0){
+            return res.status(422).json({msg: "This user already exists"})
+        }
+        const emailFinder = await authModel.findByEmail(email);
+        if(emailFinder.length > 0){
+            return res.status(422).json({msg:"This Email is already been used"})
+        }
+        await authModel.registerUser(user, password.toString(), email)
+        return res.status(200).json({msg: "Congratulations, You can use To do List Now"});
+    }catch(error){
+        console.error("Erro ao registrar:",error)
+        return res.status(500).send("Ocorreu um erro ao registrar")
     }
-    const emailFinder = await authModel.findByEmail(req.body.email);
-    if(emailFinder.length > 0){
-        return res.status(422).json({msg:"This Email is already been used"})
-    }
-    res.status(200).json({msg: "cadastrou"});
 };
 
 
--- src/models/authModel.js
+++ src/models/authModel.js
@@ -1,5 +1,8 @@
 const connect = require('./elephantsql');
-
+const bcrypt = require('bcrypt');
+//INSERIR NO BANCO DE DADOS AS SENHAS CRIPTOGRAFADAS
+//CHAT GPT JA PASSOU A CALL
+//LOGIN VAI SER NO MESMO MODEL E CONTROLLER
 const findByUser = async (user) =>{
     const client = await connect();
     const users =  await client.query(`SELECT "User", "Password", "JWT", "Email" FROM users WHERE "User" = $1;`,[user]);
@@ -14,7 +17,33 @@ const findByEmail = async (Email) =>{
     return users.rows;
 }
 
+const findByJWT = async (jwt) => {
+    const client = await connect();
+    const users =  await client.query(`SELECT "User", "Password", "JWT", "Email" FROM users WHERE "JWT" = $1;`,[jwt]);
+    client.release();
+    return users.rows;
+}
+
+const encryptPassword = async (password) => {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+}
+
+const verifyPassword = async (password,hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword);
+} 
+
+const registerUser =  async (user, password, email) => {
+    const client = await connect();
+    const hashedPassword =  await encryptPassword(password);
+    const jwt = await encryptPassword(user);
+    await client.query(`INSERT INTO users ("User", "Password", "JWT" , "Email") VALUES ($1, $2, $3, $4)`, [user, hashedPassword, jwt , email])
+    return;
+    
+};
+
 module.exports = {
     findByUser,
-    findByEmail
+    findByEmail,
+    registerUser
 };
