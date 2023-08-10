cc0b381ad39a415e6600979555799a75ef30a0fd
Natan <zerogamerptbr@gmail.com>
2023-08-10 12:55:11 -0300

    feat: active data

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -2,21 +2,21 @@ const connect = require('./elephantsql');
 
 const getAll = async(jwt) => {
     const client = await connect();
-    const tasks = await client.query('SELECT id, hora, nome, feito, "desc" FROM tasks WHERE jwt = $1', [jwt]);
+    const tasks = await client.query('SELECT id, hora, nome, feito, "desc" FROM tasks WHERE jwt = $1 AND ativo = true', [jwt]);
     client.release();
     return tasks;
 };
 
 const insertData = async(nome, hora, jwt, desc) => {
     const client = await connect();
-    await client.query('INSERT INTO tasks (nome, hora, jwt, feito, "desc") VALUES ($1, $2, $3, $4, $5)', [nome, hora, jwt, "FALSE", desc]);
+    await client.query('INSERT INTO tasks (nome, hora, jwt, feito, "desc", ativo) VALUES ($1, $2, $3, $4, $5, true)', [nome, hora, jwt, "FALSE", desc]);
     client.release();
     return;
 };
 
 const deleteData = async(nome, jwt) => {
     const client = await connect();
-    await client.query('DELETE FROM tasks WHERE nome = $1 AND jwt = $2', [nome, jwt]);
+    await client.query('UPDATE tasks SET ativo = false WHERE nome = $1 AND jwt = $2', [nome, jwt]);
     client.release();
     return;
 };
