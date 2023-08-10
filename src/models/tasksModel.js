0ce33ee8c17391d93815ccfa39f03a1bbc1e4fb4
Natan <zerogamerptbr@gmail.com>
2023-08-10 12:32:36 -0300

    Fix: models

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -9,7 +9,7 @@ const getAll = async(jwt) => {
 
 const insertData = async(nome, hora, jwt, desc) => {
     const client = await connect();
-    await client.query('INSERT INTO tasks (nome, hora, jwt, feito, desc) VALUES ($1, $2, $3, $4, $5)', [nome, hora, jwt, "FALSE", desc]);
+    await client.query('INSERT INTO tasks (nome, hora, jwt, feito, "desc") VALUES ($1, $2, $3, $4, $5)', [nome, hora, jwt, "FALSE", desc]);
     client.release();
     return;
 };
@@ -23,7 +23,7 @@ const deleteData = async(nome, jwt) => {
 
 const updateTask = async(nome, hora ,feito, id, jwt, desc) => {
     const client = await connect();
-    await client.query('UPDATE tasks SET hora = $1, nome = $2, feito = $3, desc = $4 WHERE id = $5 AND jwt = $6', [hora, nome,feito,desc,id,jwt]);
+    await client.query('UPDATE tasks SET hora = $1, nome = $2, feito = $3, "desc" = $4 WHERE id = $5 AND jwt = $6', [hora, nome,feito,desc,id,jwt]);
     client.release();
     return;
 };
